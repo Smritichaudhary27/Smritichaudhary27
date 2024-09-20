a458393d5c839c78e8119a75c6cb23c42f790d07
Smritichaudhary27 <161445034+Smritichaudhary27@users.noreply.github.com>
2024-09-20 12:40:19 +0530

    Add files via upload

---
+++ program6.cpp
@@ -0,0 +1,13 @@
+#Write a program to create an array which have n values, take user input and display it.
+#include <iostream>
+using namespace std;
+
+int n,i;
+for (i=0;i<n;i++)
+{
+cin>>a[i];
+}
+for(i=0;i<n;i++)
+{
+cout<<a[i];
+}
