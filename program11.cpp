08c07359e7679c1936c3e7aea46db6a1eb3a8fe8
Smritichaudhary27 <161445034+Smritichaudhary27@users.noreply.github.com>
2024-09-20 12:44:29 +0530

    Add files via upload

---
+++ program11.cpp
@@ -0,0 +1,14 @@
+#Write a program to find the sum of all elements of 2D array.
+#include <iostream>
+using namespace std;
+void main()
+{
+int G[2][2],i,j,s=0;
+for (i=0;i<2;i++)
+{
+for(j=0;j<2;j++)
+{
+s=sta[i][j];
+} 
+}
+}
