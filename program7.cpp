2df49638b4b511a5f846b4c6099b1e86bfdfa76e
Smritichaudhary27 <161445034+Smritichaudhary27@users.noreply.github.com>
2024-09-20 12:40:37 +0530

    Add files via upload

---
+++ program7.cpp
@@ -0,0 +1,14 @@
+#Write a program to create an 2D  array and store value from user and display it. 
+#include <iostream>
+using namespace std;
+int a[5][5];
+int i;j;
+for(i=0;i<2;i++)
+{
+for(j=0;j<2;j++)
+{
+cin>>a[i][j];
+for(i=0;i<2;i++)
+{
+cout<<a[i][j];
+}
