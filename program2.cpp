dcc6c1a5852259ff0344ea70391a821e9d85e4df
Smritichaudhary27 <161445034+Smritichaudhary27@users.noreply.github.com>
2024-09-20 12:39:00 +0530

    Add files via upload

---
+++ program2.cpp
@@ -0,0 +1,13 @@
+#Write a program to check a number is divisible by 7 or not. 
+#include <iostream>
+using namespace std;
+
+int n;
+if(n%7==0)
+{
+cout<<"number is divisible by 7";
+}
+else
+{
+cout<<"not divisible";
+}
